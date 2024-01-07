02d4be94fd83e6199254315022419d7ad2f23b06
Abdullah Khan <abdullah5601013@gmail.com>
2024-01-07 23:57:11 +0500

    Description addeds

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,9 @@ function Projects() {
               imgPath={tinDog}
               isBlog={false}
               title="TinDog"
-              description="wwwwwwwwwwwwwwww"
+              description="Demonstrated Bootstrap proficiency in Tindog clone, excelling in
+               responsive design and CSS styling. Seamless cross-device experience highlights 
+               technical prowess and creative finesse, showcasing a dedication to web development excellence."
               ghLink="https://github.com/Abdullah1013/First-website-structure"
               demoLink="https://abdullah1013.github.io/First-website-structure/"
             />
@@ -38,7 +40,11 @@ function Projects() {
               imgPath={ecomerce}
               isBlog={false}
               title="E-commerce"
-              description="wwwwwwwwwwwww"
+              description="Crafted an engaging e-commerce site, leveraging jQuery,
+               JavaScript, and Bootstrap for responsiveness. Enriched user experience 
+               with dynamic animations, showcasing a blend of technical innovation and design
+                finesse. Explore the project to witness seamless functionality and a visually 
+                appealing online shopping experience"
               ghLink="https://github.com/Abdullah1013/First-e-commerice-web"
               demoLink="https://abdullah1013.github.io/First-e-commerice-web/"
             />
@@ -49,7 +55,11 @@ function Projects() {
               imgPath={packagingRack}
               isBlog={false}
               title="Packaging Rack"
-              description="wwwwwwwwwwww"
+              description="Delivered a responsive website for a packaging racks
+               company, enabling users to view and order products effortlessly. 
+               Implemented a seamless user interface, ensuring accessibility and 
+               functionality across devices. Explore the site to experience a blend 
+               of design finesse and user-friendly features, enhancing the client's online presence."
               ghLink="https://github.com/Abdullah1013/My-pacakaging-rack"
               demoLink="https://abdullah1013.github.io/My-pacakaging-rack/"              
             />
@@ -60,7 +70,11 @@ function Projects() {
               imgPath={diceGame}
               isBlog={false}
               title="Dice Game"
-              description="wwwwwwwwwww"
+              description="Designed and implemented a dynamic multiplayer
+               dice game as a practice project where two players engage in a thrilling contest.
+                The player with the highest dice value emerges victorious,
+                 showcasing a playful combination of JavaScript interactivity
+                  and engaging user experience. Experience the excitement firsthand by exploring this project."
               ghLink="https://github.com/Abdullah1013/dice_game"
               demoLink="https://abdullah1013.github.io/dice_game/"
             />
@@ -71,7 +85,11 @@ function Projects() {
               imgPath={wordSearch}
               isBlog={false}
               title="Word Search Game"
-              description="wwwwwwwwwww"
+              description="Created an engaging word search game in C++, offering
+               multiple difficulty levels for players to choose from. Challenge yourself
+                by guessing the missing alphabets in the presented words, showcasing both 
+                problem-solving skills and C++ console-based programming expertise. Explore 
+                this text-based project for an interactive and educational gaming experience."
               ghLink="https://github.com/Abdullah1013/Word_search_game"
              demoLink="https://www.linkedin.com/posts/abdullah-khan-126122227_word-puzzle-game-using-c-second-semester-activity-6961743209379061760--Zov?utm_source=share&utm_medium=member_desktop" 
             />
